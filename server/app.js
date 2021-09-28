2243fd39b129788b8f70933839af4d37e6d9d603
cbmcdougall <c_mcdougall97@yahoo.co.uk>
2021-09-28 15:49:13 +0100

    add route to retrieve results from search string

--- server/app.js
+++ server/app.js
@@ -1,16 +1,31 @@
-// require the lib we'll use
 const express = require('express');
 const cors = require('cors');
+const data = require('./data.json')
 const port = 3000;
 
 // create server
 const app = express();
-app.use(express.json());
+// app.use(express.json());
 app.use(cors());
 
+function findResults(search){
+    // Searches for the inputted search string in the bodyText values in the data array, returning the parent Objects
+    const results = data.filter(item => item.bodyText.toLowerCase().includes(search))
+    return results;
+};
+
+// Home route (to test server is working)
 app.get('/', (req, res) => {
     res.send('Hello World!');
-})
+});
+
+// On request, search for searchterm and return 10 results
+app.get('/:search', (req, res) => {
+    const searchTerm = req.params.search.toLowerCase();
+    const results = findResults(searchTerm).slice(0,10);
+    res.json(results);
+});
+
 
 // start server
 app.listen(port, () => {
