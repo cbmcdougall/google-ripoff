da66379ff975a86628e39c0840e5b480aaf13017
Katie Chedgzoy <katieched98@hotmail.co.uk>
2021-09-28 17:23:01 +0100

    create functions to get search results

--- client/static/js/index.js
+++ client/static/js/index.js
@@ -1,6 +1,33 @@
-//event listner for clicking search button
-//make request to api, return json object?
+const searchButton = document.querySelector('#search');
+const surpriseButton = document.querySelector('#surprise');
+
+const searchBar = document.getElementById('searchBar');
+
+//function to show 10 search results
+function getSearchResult(e) {
+    const searchString = searchBar.value;
+
+    //fetch(`http://localhost:3000/${searchString}`)
+    //    .then(resp => resp.json())
+    //    .then(resp[2].link)
 
 
-//event listener for clicking random button
-//make request to api, return json object?
+}
+
+//function to show a random result
+function getSurpriseResult(e) {
+    const searchString = searchBar.value;
+
+//     fetch(`http://localhost:3000/${searchString}/random`)
+//        .then(resp => )
+}
+
+searchButton.addEventListener('click', e => {
+    e.preventDefault();
+    getSearchResult(e);
+});
+
+surpriseButton.addEventListener('click', e => {
+    e.preventDefault();
+    getSurpriseResult(e);
+});
