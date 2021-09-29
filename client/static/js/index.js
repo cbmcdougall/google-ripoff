c5267709456be55f4b9079f5fe20d8a48cef59c0
Katie Chedgzoy <katieched98@hotmail.co.uk>
2021-09-29 11:09:18 +0100

    add logo

--- client/static/js/index.js
+++ client/static/js/index.js
@@ -1,33 +1,44 @@
-const searchButton = document.querySelector('#search');
-const surpriseButton = document.querySelector('#surprise');
+const searchButton = document.getElementById('search');
+const surpriseButton = document.getElementById('surprise');
 const searchBar = document.getElementById('searchBar');
 const searchEngine = document.getElementById('searchEngine')
 const searchResults = document.getElementById('results');
 
-//headers
-const firstHeader = document.getElementById('firstHeader');
-
 //function to show 10 search results
 function getSearchResult(e) {
+    //handling spaces in searches
     const searchString = searchBar.value.replace(/\s/g, "_");
 
+    //hiding search bar and showing results
     searchEngine.className = "hide";
+    searchResults.className = "";
 
     fetch(`http://localhost:3000/${searchString}`)
        .then(resp => resp.json())
-       .then(resp => { //loop through all array elements
-            for(let i = 0; i < resp.length; i++)
+       .then(resp => { 
+            for(let i = 0; i < resp.length; i++) //loop through all array elements
             {
-                searchResults.append(
-                    `<a href="${resp[i].link}">${resp[i].header}</a>` +
-                    `<p>${resp[i].bodyText}</p>`
-                );
-            }
-            
+                //adding website name to search results
+                const websiteName = document.createElement("A");
+                const websiteNameText = document.createTextNode(`${resp[i].header}`);
+                websiteName.appendChild(websiteNameText);
+                searchResults.appendChild(websiteName);
+                
+                //adding website info to search results
+                const websiteInfo = document.createElement("P");
+                const websiteInfoText = document.createTextNode(`${resp[i].bodyText}`);
+                websiteInfo.appendChild(websiteInfoText);
+                searchResults.appendChild(websiteInfo);
+
+
+                // const headerText = `<a href="${resp[i].link}">${resp[i].header}</a>`;
+                // const bodyText = `<p>${resp[i].bodyText}</p>`;
+                // searchResults.append(headerText, bodyText);
+            }; 
         })
-        // .catch(error => {
-        //     error = error.json();
-        // });
+        .catch(error => {
+            error = error.json();
+        });
 }
 
 //function to show a random result
